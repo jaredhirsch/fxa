b60cd8e00ac88bd595f63f6688dcfe2afe971023
Jared Hirsch <ohai@6a68.net>
2021-03-19 09:00:40 -0700

    get delete_account tests working, refactor shared setup into beforeEach

--- packages/fxa-content-server/tests/functional/settings_v2/delete_account.js
+++ packages/fxa-content-server/tests/functional/settings_v2/delete_account.js
@@ -2,28 +2,27 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- 'use strict';
+'use strict';
 
- const { describe, it, beforeEach } = intern.getPlugin('interface.bdd');
- const selectors = require('../lib/selectors');
- const FunctionalHelpers = require('../lib/helpers');
- const FunctionalSettingsHelpers = require('./lib/helpers');
- const { navigateToSettingsV2 } = FunctionalSettingsHelpers;
- const password = 'passwordzxcv';
+const { describe, it, beforeEach } = intern.getPlugin('interface.bdd');
+const selectors = require('../lib/selectors');
+const FunctionalHelpers = require('../lib/helpers');
+const FunctionalSettingsHelpers = require('./lib/helpers');
+const { navigateToSettingsV2 } = FunctionalSettingsHelpers;
+const password = 'passwordzxcv';
 
- const {
-   click,
-   testElementExists,
-   type,
-   visibleByQSA,
- } = FunctionalHelpers.helpersRemoteWrapped;
+const {
+  click,
+  testElementExists,
+  type,
+  visibleByQSA,
+} = FunctionalHelpers.helpersRemoteWrapped;
 
 describe('Delete account', () => {
   beforeEach(async ({ remote }) => {
     await navigateToSettingsV2(remote);
-  });
 
-  /* it('can delete account', async ({ remote }) => {
+    // Click the delete button
     await click(
       selectors.SETTINGS_V2.DELETE_ACCOUNT.DELETE_ACCOUNT_BUTTON,
       remote
@@ -32,40 +31,25 @@ describe('Delete account', () => {
       selectors.SETTINGS_V2.DELETE_ACCOUNT.DELETE_ACCOUNT_MODAL,
       remote
     );
-
     // Check all the checkboxes before continuing
-    await click(
-      selectors.SETTINGS_V2.DELETE_ACCOUNT.CHECKBOX_1,
+    const labels = await remote.findAllByCssSelector(
+      selectors.SETTINGS_V2.DELETE_ACCOUNT.CHECKBOXES,
       remote
     );
-    await click(
-      selectors.SETTINGS_V2.DELETE_ACCOUNT.CHECKBOX_2,
-      remote
-    );
-    await click(
-      selectors.SETTINGS_V2.DELETE_ACCOUNT.CHECKBOX_3,
-      remote
-    );
-    await click(
-      selectors.SETTINGS_V2.DELETE_ACCOUNT.CHECKBOX_4,
-      remote
-    );
+    labels.map((label) => label.click());
+    await remote.end();
 
     // Click continue
-    await click(
-      selectors.SETTINGS_V2.DELETE_ACCOUNT.CONTINUE_BUTTON,
-      remote
-    );
+    await click(selectors.SETTINGS_V2.DELETE_ACCOUNT.CONTINUE_BUTTON, remote);
+  });
 
+  it('can delete account', async ({ remote }) => {
     // Enter incorrect password
     await testElementExists(
       selectors.SETTINGS_V2.DELETE_ACCOUNT.DELETE_ACCOUNT_MODAL,
       remote
     );
-    await click(
-      selectors.SETTINGS_V2.DELETE_ACCOUNT.PASSWORD_LABEL,
-      remote
-    );
+    await click(selectors.SETTINGS_V2.DELETE_ACCOUNT.PASSWORD_LABEL, remote);
     await type(
       selectors.SETTINGS_V2.DELETE_ACCOUNT.PASSWORD_INPUT,
       'invalid password',
@@ -81,10 +65,7 @@ describe('Delete account', () => {
     );
 
     // Enter correct password
-    await click(
-      selectors.SETTINGS_V2.DELETE_ACCOUNT.PASSWORD_LABEL,
-      remote
-    );
+    await click(selectors.SETTINGS_V2.DELETE_ACCOUNT.PASSWORD_LABEL, remote);
     await type(
       selectors.SETTINGS_V2.DELETE_ACCOUNT.PASSWORD_INPUT,
       password,
@@ -94,35 +75,14 @@ describe('Delete account', () => {
       selectors.SETTINGS_V2.DELETE_ACCOUNT.DELETE_CONFIRM_BUTTON,
       remote
     );
-  }) */
+  });
 
   it('cancel delete account', async ({ remote }) => {
-    await click(
-      selectors.SETTINGS_V2.DELETE_ACCOUNT.DELETE_ACCOUNT_BUTTON,
-      remote
-    );
-
-    // Check all the checkboxes before continuing
-    await findAllByCssSelector(
-      selectors.SETTINGS_V2.DELETE_ACCOUNT.CHECKBOXES
-    );
-    (labels) => labels.map((label) => label.click())
-    end();
-
-    // Click continue
-    await click(
-      selectors.SETTINGS_V2.DELETE_ACCOUNT.CONTINUE_BUTTON,
-      remote
-    );
-    await click(
-      selectors.SETTINGS_V2.DELETE_ACCOUNT.PASSWORD_LABEL,
-      remote
-    );
+    await click(selectors.SETTINGS_V2.DELETE_ACCOUNT.PASSWORD_LABEL, remote);
     await type(
       selectors.SETTINGS_V2.DELETE_ACCOUNT.PASSWORD_INPUT,
       password,
       remote
     );
-    });
-
   });
+});
